ac8a81ddff30794ef85fc0fce7339d6c349767aa
jovemOG-Dev <gabriel.silvagomes2006@gmail.com>
2026-01-22 10:28:11 -0300

    ts minTomin

--- generate-readme.js
+++ generate-readme.js
@@ -3,26 +3,25 @@ import fs from "fs";
 // Lê o template
 const template = fs.readFileSync("README.template.md", "utf-8");
 
-// Hora local do fuso escolhido
+// Hora local SP
 const now = new Date(
   new Date().toLocaleString("en-US", {
     timeZone: "America/Sao_Paulo"
   })
 );
 
-const hour = now.getHours();
+const minute = now.getMinutes();
+
+const slot = minute % 3;
 
 /**
- * Períodos:
- * Manhã: 05–12
- * Tarde: 13–18
- * Noite: 19–04
+ * Rotação rápida (10 em 10 minutos)
  */
 let banner;
 
-if (hour >= 5 && hour <= 12) {
+if (slot === 0) {
   banner = "./assets/banner-day.gif";
-} else if (hour >= 13 && hour <= 18) {
+} else if (slot === 1) {
   banner = "./assets/banner-afternoon.gif";
 } else {
   banner = "./assets/banner-night.gif";
